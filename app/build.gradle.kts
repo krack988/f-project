8690b0fd84abc11d315a29ba37f87d246967dd49
Mihail P <krack988@gmail.com>
2024-09-03 17:34:58 +0300

    fix hilt dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
     kotlin("kapt")
+    id ("com.google.dagger.hilt.android")
 }
 
 android {
